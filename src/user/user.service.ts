8670e9bf3ffa2188152e8f0f30a87be2d65f74ec
Wanderleyps <wanderleyps794@gmail.com>
2024-10-28 21:56:32 -0300

    feat: insert UserEntity

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,26 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { hash } from 'bcrypt';
+import { Repository } from 'typeorm';
 import { createUserDto } from './dtos/createUser.dto';
-import { User } from './interfaces/user.interface';
+import { UserEntity } from './entities/user.entity';
 
 @Injectable()
 export class UserService {
-  private users: User[] = [];
-  async createUser(createUserDto: createUserDto): Promise<User> {
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+  ) {}
+
+  async createUser(createUserDto: createUserDto): Promise<UserEntity> {
     const saltOrRounds = 10;
     const passwordHashed = await hash(createUserDto.password, saltOrRounds);
-    const user: User = {
+
+    return this.userRepository.save({
       ...createUserDto,
-      id: this.users.length + 1,
       password: passwordHashed,
-    };
-
-    this.users.push(user);
-
-    return user;
+    });
   }
 
-  async getAllUsers(): Promise<User[]> {
-    return this.users;
+  async getAllUsers(): Promise<UserEntity[]> {
+    return this.userRepository.find();
   }
 }
