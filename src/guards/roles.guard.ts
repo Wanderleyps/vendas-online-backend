4bedd03a06ec039582f983c36575dfe8828dedf5
Wanderleyps <wanderleyps794@gmail.com>
2024-11-18 21:18:22 -0300

    info: add comments

--- src/guards/roles.guard.ts
+++ src/guards/roles.guard.ts
@@ -13,27 +13,34 @@ export class RolesGuard implements CanActivate {
   ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
+    // Obtém os papéis (roles) necessários para acessar esta rota.
     const requiredRoles = this.reflector.getAllAndOverride<UserType[]>(
       ROLES_KEY,
       [context.getHandler(), context.getClass()],
     );
 
+    // Se não houver papéis definidos, permite o acesso.
     if (!requiredRoles) {
       return true;
     }
 
+    // Extrai o token de autenticação do cabeçalho da requisição.
     const { authorization } = context.switchToHttp().getRequest().headers;
 
+    // Tenta verificar o token JWT e extrair as informações do usuário logado.
+    // Se houver algum erro na verificação, captura e retorna undefined.
     const loginPayload: LoginPayload | undefined = await this.jwtService
       .verifyAsync(authorization, {
         secret: process.env.JWT_SECRET,
       })
       .catch(() => undefined);
 
+    // Se o token for inválido ou não existir, nega o acesso.
     if (!loginPayload) {
       return false;
     }
 
+    // Verifica se algum dos papéis necessários está presente nos papéis do usuário.
     const hasPermission = requiredRoles.some(
       (role) => role === loginPayload.typeUser,
     );
